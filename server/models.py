d08e822e6dd1e9e24f6ce0b76ba6d149878a52cc
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-07-11 12:13:01 +0300

    Fix tables

--- server/models.py
+++ server/models.py
@@ -44,6 +44,8 @@ class Booking(db.Model, SerializerMixin):
 class Service(db.Model, SerializerMixin):
     __tablename__ = 'services'
 
+    serialize_rules = ('-reviews', '-bookings', '-bookingservices')
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(80))
